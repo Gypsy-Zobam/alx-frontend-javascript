6d4be33959fe080bfd6a167b9f63a10c5e120741
Gypsy-Zobam <zo_bam@yahoo.com>
2023-06-27 04:51:27 +0000

    10-loops.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,12 @@
 export default function createIteratorObject(report) {
   const workers = [];
-  /* eslint-disable var is not ES6-friendly. */
+  /* eslint-disable no-unused-vars */
   for (const [department, employees] of Object.entries(report.allEmployees)) {
     for (const employe of employees) {
       workers.push(employe);
     }
   }
-  /* eslint-enable var is not ES6-friendly. */
+  /* eslint-enable no-unused-vars */
 
   return workers;
 }
