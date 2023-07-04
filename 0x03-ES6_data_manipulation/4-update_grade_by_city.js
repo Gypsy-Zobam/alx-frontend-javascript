eda6461465b62012ffde0116492352830e31d76b
Gypsy-Zobam <zo_bam@yahoo.com>
2023-07-04 13:38:44 +0000

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(listStudent, city, newGrades) {
-  return array.filter((i) => i.location === city).map((listStudent) => {
-      const gradeI = newGrades.filter((i) => i.studentId === listStudent.id).map((x) => x.grade)[0];
+  return array.filter((obj) => obj.location === city).map((listStudent) => {
+      const gradeI = newGrades.filter((obj) => obj.studentId === listStudent.id).map((obj) => obj.grade)[0];
       const grade = gradeI || 'N/A';
       return { ...student, grade };
     });
