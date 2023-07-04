6f2043e17b8ae201ec30e5901465b777131c63e1
Gypsy-Zobam <zo_bam@yahoo.com>
2023-07-04 14:42:49 +0000

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,5 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  for (const prouduct of map) if (product[1] === 1) map.set(product[0], 100);
+  return map;
+}
