d53e72a8be6e902eb686338a6e0ff43d6c8ddd65
Gypsy-Zobam <zo_bam@yahoo.com>
2023-07-04 14:30:45 +0000

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const Products = new Map();
+  Products.set('Apples', 10);
+  Products.set('Tomatoes', 10);
+  Products.set('Pasta', 1);
+  Products.set('Rice', 1);
+  Products.set('Banana', 5);
+
+  return Products;
+}
